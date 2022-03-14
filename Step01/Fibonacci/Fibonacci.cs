cc9ba4282f69a0d505e222eb9ea7b44b476ff460
Hootan Hemmati <hootanhemmati@outlook.com>
2022-03-14 11:34:49 +0330

    Add and implantation Fibonacci algorithm

---
+++ Step01/Fibonacci/Fibonacci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class Fibonacci
+    {
+        /// <summary>
+        /// Calculate Fibonacci algorithm
+        /// </summary>
+        /// <param name="numberOfCalculate">Number of algorithm you want to calculate</param>
+        /// <returns>Return all sentences of Fibonacci algorithm</returns>
+        public static int[] CalculateFibonacciAlgorithm(int numberOfCalculate)
+        {
+            //initial result array
+            int[] result = new int[numberOfCalculate];
+
+            //implementation first value = 0
+            result[0] = 0;
+
+            //implementation first value = 1
+            result[1] = 1;
+
+            ////implementation other value
+            for (int i = 2; i < numberOfCalculate; i++)
+            {
+                result[i] = result[i-1] + result[i - 2];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Step01/Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    #region Main Method
+    public static void Main(string[] args)
+    {
+        //Calculate and print Fibonacci algorithm with sample value of 14
+        PrintArray(Fibonacci.Fibonacci.CalculateFibonacciAlgorithm(14));
+    }
+    #endregion
+
+    #region Print Array
+    /// <summary>
+    /// Print array in output
+    /// </summary>
+    /// <param name="array">An array you want to print in output</param>
+    public static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + "\t");
+        }
+    }
+    #endregion
+}
