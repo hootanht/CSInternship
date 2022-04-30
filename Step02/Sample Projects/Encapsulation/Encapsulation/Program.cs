d9f64d3cf6e84c87da52723325b0c87246ac4d60
Hootan Hemmati <hootanhemmati@outlook.com>
2022-04-30 21:26:12 +0430

    add Encapsulation

---
+++ Step02/Sample Projects/Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,13 @@
+﻿using Encapsulation;
+
+User u = new User();
+// set accessors will invoke
+u.Name = "Suresh Dasari";
+// set accessors will invoke
+u.Location = "Hyderabad";
+// get accessors will invoke
+Console.WriteLine("Name: " + u.Name);
+// get accessors will invoke
+Console.WriteLine("Location: " + u.Location);
+Console.WriteLine("\nPress Enter Key to Exit..");
+Console.ReadLine();
---
+++ Step02/Sample Projects/Encapsulation/Encapsulation/User.cs
@@ -0,0 +1,37 @@
+﻿namespace Encapsulation
+{
+    /// <summary>
+    /// Sample class for testing the Encapsulation
+    /// </summary>
+    public class User
+    {
+        #region Private fields
+        // private field for the location of the user
+        private string location;
+        // private field for the name of the user
+        private string name;
+        #endregion
+
+        #region Sets The Location
+        /// <summary>
+        /// Gets or sets the location of the user
+        /// </summary>
+        public string Location
+        {
+            get => location;
+            set => location = value;
+        }
+        #endregion
+
+        #region Sets The Name
+        /// <summary>
+        /// Gets or sets the name of the user
+        /// </summary>
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        #endregion
+    }
+}
