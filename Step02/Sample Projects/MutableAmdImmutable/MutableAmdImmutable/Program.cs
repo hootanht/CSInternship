2afe4383aabea0fbb3256f220366e0ff3fa12421
Hootan Hemmati <hootanhemmati@outlook.com>
2022-05-02 18:14:07 +0430

    add Mutable And Immutable

---
+++ Step02/Sample Projects/MutableAmdImmutable/MutableAmdImmutable/Program.cs
@@ -0,0 +1,32 @@
+﻿/// <summary>
+/// This is the class with immutable property
+/// </summary>
+public class SampleClass
+{
+    // Immutable field
+    private readonly string myString;
+
+    /// <summary>
+    /// This is the constructor to initialize the value of immutable field
+    /// </summary>
+    /// <param name="str">This is default value of immutable field</param>
+    public SampleClass(string str)
+    {
+        myString = str;
+    }
+
+    /// <summary>
+    /// This is the property to get the value of immutable field
+    /// </summary>
+    public string GetStr => myString;
+
+    // Another shape => public string GetStr { get { return myString; } }
+}
+public partial class Program
+{
+    public static void Main()
+    {
+        SampleClass sampleClass = new SampleClass("Hootan");
+        Console.WriteLine(sampleClass.GetStr);
+    }
+}
